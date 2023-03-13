01807d87bcf2426e44767a8a60d7c49a286bbbfb
jwlam2020 <119370104+jwlam2020@users.noreply.github.com>
2023-03-13 18:58:42 -0400

    Update external.js

--- external.js
+++ external.js
@@ -27,14 +27,14 @@ function myTimer() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("demo").innerHTML = days + ":" + hours + ":"
+    + minutes + ":" + seconds + ":";
 
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
       document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-  }, 1000);
+      }
+    }, 1000);
   
 }
